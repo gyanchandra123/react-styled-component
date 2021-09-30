99924b216be956c2a8ef02af19f882ee9b8c8905
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-30 21:45:09 +0530

    applying inline-style  for the 1st time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import styled from 'styled-components'
+
+const BasicStyle = styled.h1`
+  text-align: center;
+  text-transform: uppercase;
+`
+
 function App() {
   return (
-    <div style={{ padding: '2rem', backgroundColor: 'grey' }}>
-      <h1 className='title'>styled components</h1>
+    <div style={{ padding: '2rem' }}>
+      <BasicStyle className='title'>styled components</BasicStyle>
       <button className='btn'>click me</button>
     </div>
   )
