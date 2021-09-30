81bb2e72fd1398a9c401db748506c058a27d63eb
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-30 21:58:56 +0530

    styling button

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ const BasicStyle = styled.h1`
   text-transform: uppercase;
 `
 
+const DefaultButton = styled.button`
+  background-color: blue;
+  border: none;
+  color: white;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  text-transform: capitalize;
+  padding: 0.25rem;
+  display: block;
+  width: 200px;
+  margin: 1rem auto;
+`
+
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <BasicStyle className='title'>styled components</BasicStyle>
-      <button className='btn'>click me</button>
+      <DefaultButton className='btn'>click me</DefaultButton>
     </div>
   )
 }
