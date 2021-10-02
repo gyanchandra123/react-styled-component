562cc7f9f153ded772d97fa29b15fdab4d666a76
gyanchandra moirangthem <gyanchandra moirangthem>
2021-10-02 12:26:19 +0530

    How to apply styles for specific color for specific values based on the condition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components/macro'
-import List from './components/List'
+import Products from './components/Products'
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <List />
+      <Products />
     </div>
   )
 }
